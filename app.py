b4df1a03198cb4039245dbdc00fb43f668499a89
corleone113 <corleonexiao@qq.com>
2023-03-11 14:06:43 +0800

    跑通demo，添加chat功能支持。

--- app.py
+++ app.py
@@ -11,25 +11,23 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 def index():
     if request.method == "POST":
         animal = request.form["animal"]
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role": "user", "content": animal}],
+            max_tokens=300,
+            temperature=0.9,
         )
-        return redirect(url_for("index", result=response.choices[0].text))
+        print(response)
+        return redirect(url_for("index", result=response.choices[0].message.content))
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
 
 
 def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
+    return """The following is a conversation with ChatGPT. ChatGPT is friendly and helpful.
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
+Human: {}
+ChatGPT:""".format(
         animal.capitalize()
     )
